f9a4c67735e82635d70f5bc70882a5e53fa47a1b
Abbalmaseda <anabelenbalmasedaa@gmail.com>
2026-04-02 12:30:38 +0200

    borrador

---
+++ variable.py
@@ -0,0 +1,21 @@
+BOARD_SIZE = 10
+
+SHIPS = {
+    "Lancha": 1,
+    "Destructor": 2,
+    "Submarino": 3,
+    "Acorazado": 4
+}
+
+WATER = "~"
+SHIP = "S"
+HIT = "X"
+MISS = "O"
+
+WELCOME_MSG = "Bienvenido a Hundir la Flota"
+TURN_MSG = "Introduce coordenadas X Y: "
+HIT_MSG = "Tocado"
+MISS_MSG =  "Agua"
+WIN_MSG = "Has ganado"
+LOSE_MSG = "Has perdido"
+
